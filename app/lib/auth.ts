3d4a375ec266312d5359d9b5fdbb37210ec59526
Aditya <adityachauhan2501@gmail.com>
2024-05-13 23:50:15 +0530

    landing-login-page

---
+++ app/lib/auth.ts
@@ -0,0 +1,73 @@
+import { PrismaClient } from "@prisma/client";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+
+const prisma = new PrismaClient();
+
+export const authOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        phone: {
+          label: "Phone number",
+          type: "text",
+          placeholder: "1231231231",
+          required: true,
+        },
+        password: { label: "Password", type: "password", required: true },
+      },
+
+      async authorize(credentials: { phone: string; password: string }) {
+        const hashedPassword = await bcrypt.hash(credentials.password, 10);
+
+        const existingUser = await prisma.user.findFirst({
+          where: {
+            number: credentials.phone,
+          },
+        });
+
+        if (existingUser) {
+          const passwordValidation = await bcrypt.compare(
+            credentials.password,
+            existingUser.password
+          );
+          if (passwordValidation) {
+            return {
+              id: existingUser.id.toString(),
+              name: existingUser.name,
+              email: existingUser.number,
+            };
+          }
+          return null;
+        }
+
+        try {
+          const user = await prisma.user.create({
+            data: {
+              number: credentials.phone,
+              password: hashedPassword,
+            },
+          });
+
+          return {
+            id: user.id.toString(),
+            name: user.name,
+            email: user.number,
+          };
+        } catch (e) {
+          console.error(e);
+        }
+
+        return null;
+      },
+    }),
+  ],
+  secret: process.env.JWT_SECRET || "secret",
+  callbacks: {
+    async session({ token, session }: any) {
+      session.user.id = token.sub;
+      return session;
+    },
+  },
+};
